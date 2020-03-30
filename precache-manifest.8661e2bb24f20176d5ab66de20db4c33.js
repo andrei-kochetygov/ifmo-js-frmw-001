7463e3aa93143174feede922c32ac1306a0e888f
flatdragon <flatdragon@yandex.ru>
2020-03-30 10:50:21 +0000

    Deploying to gh-pages from  - a9ecfbc294dc4ec073b9ea71234244e9f5d05980 🚀

---
+++ precache-manifest.8661e2bb24f20176d5ab66de20db4c33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8fa3b42bb0e034db0c05b5fa2aa221f",
+    "url": "/ifmo-js-frmw-001/index.html"
+  },
+  {
+    "revision": "40d3287fc72960e63de2",
+    "url": "/ifmo-js-frmw-001/static/css/main.9bfcfb91.chunk.css"
+  },
+  {
+    "revision": "2f902e75cbfd3baf7925",
+    "url": "/ifmo-js-frmw-001/static/js/2.9dfbaab0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ifmo-js-frmw-001/static/js/2.9dfbaab0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40d3287fc72960e63de2",
+    "url": "/ifmo-js-frmw-001/static/js/main.f4844291.chunk.js"
+  },
+  {
+    "revision": "a6190962d3f987283ec7",
+    "url": "/ifmo-js-frmw-001/static/js/runtime-main.42ddf4c7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ifmo-js-frmw-001/static/media/logo.5d5d9eef.svg"
+  }
+]);
